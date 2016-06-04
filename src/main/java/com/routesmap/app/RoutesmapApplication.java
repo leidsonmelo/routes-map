270ccff3c7f8ea245df8e013767deb5a2e55c3e9
Leidson Melo <leidson.melo@gmail.com>
2016-06-04 00:05:52 -0300

    Starting project

---
+++ src/main/java/com/routesmap/app/RoutesmapApplication.java
@@ -0,0 +1,12 @@
+package com.routesmap.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutesmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutesmapApplication.class, args);
+	}
+}
